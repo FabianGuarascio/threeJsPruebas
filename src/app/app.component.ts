e507cdf6d7a18fac657ebb17b4e532eae8d27054
Fabian <guarascio.fabiand@gmail.com>
2023-05-13 15:29:15 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'threeJsPrueba';
+  @ViewChild("element") element?: ElementRef<HTMLDivElement>;
+  private renderer!: THREE.WebGLRenderer;
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
+  private controls!: OrbitControls;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.initThree();
+    this.addCube();
+    this.addLight();
+    this.animate();
+  }
+
+  private initThree(): void {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, this.element!.nativeElement.offsetWidth / this.element!.nativeElement.offsetHeight, 0.1, 1000);
+    this.camera.position.z = 5;
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(this.element!.nativeElement.offsetWidth, this.element!.nativeElement.offsetHeight);
+    this.element?.nativeElement.appendChild(this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+  }
+
+  private addCube(): void {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+  }
+
+  private addLight(): void {
+    const light = new THREE.PointLight(0xffffff, 1, 100);
+    light.position.set(0, 0, 10);
+    this.scene.add(light);
+  }
+
+  private animate(): void {
+    requestAnimationFrame(() => this.animate());
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+  }
+
+
 }
